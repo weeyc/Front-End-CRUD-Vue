023bc58c3556b7470f6cff8d45695d7fb20291b2
zeiod <76787324+weeyc@users.noreply.github.com>
2021-11-19 10:26:27 +0800

    Finalize V1.1

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -2,21 +2,10 @@
 @extends('layouts.app')
 @section('content')
 
-        {{-- <div class="container mx-auto flex justify-between">
-            <div class="flex">
-                <router-link class="mr-4" to='/' exact>Home</router-link>
-                <router-link to='/about'>About</router-link>
-            </div>
-            <div class="flax">
-                <router-link class="mr-4" to='/login' exact>Login</router-link>
-                <router-link to='/register'>Register</router-link>
-            </div>
-        </div> --}}
 <div id="app">
-    <top-bar> </top-bar>
 
+    <top-bar> </top-bar>
     <table-body></table-body>
-    <router-view></router-view>
 
 </div>
 
